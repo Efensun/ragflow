d0bec411c74dd2f3a8512b75ec8587850fb59ade
Efensun <efen.sun@vinotech.com>
2025-07-24 16:28:57 +0800

    fix日志输出

--- api/jobs/sync_clickup_doc.py
+++ api/jobs/sync_clickup_doc.py
@@ -17,16 +17,20 @@ from api.utils.file_utils import filename_type
 from rag.utils.storage_factory import STORAGE_IMPL
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s',
-    handlers=[
-        logging.FileHandler('./clickup_sync.log', encoding='utf-8'),
-        logging.StreamHandler()
-    ]
-)
-
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+if not logger.handlers:
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s')
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+
+logger.info("=" * 50)
+logger.info("ClickUp文档同步脚本启动")
+logger.info("=" * 50)
 
 COINEX_WEB_FOLDER_ID = ''
 COINEX_PRODUCT_FOLDER_ID = ''
